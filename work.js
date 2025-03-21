43f02246eb3cd4f79af91dfd7c421032e258f3f9
rakibsiam4444 <rakibsiam4444@gmail.com>
2025-03-21 23:55:49 +0600

    Add files via upload

---
+++ work.js
@@ -0,0 +1,207 @@
+let inNumber=document.getElementById('increase-number').innerText;
+let inNumb=parseFloat(inNumber);
+let decNumber=document.getElementById('decrease-number').innerText;
+let numb=parseFloat(decNumber);
+
+
+
+document.getElementById('complete').addEventListener('click',function(){
+
+inNumb++;
+numb--;
+alert("Board updated successfully");
+if(inNumb==29){
+alert("successful")
+}
+increaseNum=document.getElementById("decrease-number").innerText=numb;
+increaseNum=document.getElementById("increase-number").innerText=inNumb;
+const tite=document.getElementById('title1').innerText;
+let now = new Date();
+
+const newComment='You have completed the task '+tite+' issue at'+ now;
+
+
+const commentContainer=document.getElementById('comment-box');
+const newTransform=document.createElement('p');
+newTransform.classList.add('comment');
+newTransform.innerText=newComment;
+///new child
+commentContainer.appendChild(newTransform);
+
+let button = document.getElementById("complete");
+button.disabled = true;
+button.classList.add("bg-gray-400", "cursor-not-allowed");
+
+
+
+
+})
+document.getElementById('complete6').addEventListener('click',function(){
+
+inNumb++;
+numb--;
+alert("Board updated successfully");
+if(inNumb==29){
+alert("successful")
+}
+increaseNum=document.getElementById("decrease-number").innerText=numb;
+increaseNum=document.getElementById("increase-number").innerText=inNumb;
+const tite6=document.getElementById('title1').innerText;
+let now = new Date();
+
+const newComment='You have completed the task '+tite6+' issue at'+ now;
+
+
+const commentContainer=document.getElementById('comment-box');
+const newTransform=document.createElement('p');
+newTransform.classList.add('comment');
+newTransform.innerText=newComment;
+///new child
+commentContainer.appendChild(newTransform);
+
+let button = document.getElementById("complete6");
+button.disabled = true;
+button.classList.add("bg-gray-400", "cursor-not-allowed");
+
+
+
+
+})
+document.getElementById('complete4').addEventListener('click',function(){
+
+inNumb++;
+numb--;
+alert("Board updated successfully");
+if(inNumb==29){
+alert("successful")
+}
+increaseNum=document.getElementById("decrease-number").innerText=numb;
+increaseNum=document.getElementById("increase-number").innerText=inNumb;
+const tite4=document.getElementById('title1').innerText;
+let now = new Date();
+
+const newComment='You have completed the task '+tite4+' issue at'+ now;
+
+
+const commentContainer=document.getElementById('comment-box');
+const newTransform=document.createElement('p');
+newTransform.classList.add('comment');
+newTransform.innerText=newComment;
+///new child
+commentContainer.appendChild(newTransform);
+
+let button = document.getElementById("complete4");
+button.disabled = true;
+button.classList.add("bg-gray-400", "cursor-not-allowed");
+
+
+
+
+})
+document.getElementById('complete3').addEventListener('click',function(){
+
+inNumb++;
+numb--;
+alert("Board updated successfully");
+if(inNumb==29){
+alert("successful")
+}
+increaseNum=document.getElementById("decrease-number").innerText=numb;
+increaseNum=document.getElementById("increase-number").innerText=inNumb;
+const tite3=document.getElementById('title3').innerText;
+let now = new Date();
+
+const newComment='You have completed the task '+tite3+' issue at'+ now;
+
+
+const commentContainer=document.getElementById('comment-box');
+const newTransform=document.createElement('p');
+newTransform.classList.add('comment');
+newTransform.innerText=newComment;
+///new child
+commentContainer.appendChild(newTransform);
+
+let button = document.getElementById("complete3");
+button.disabled = true;
+button.classList.add("bg-gray-400", "cursor-not-allowed");
+
+
+
+
+})
+document.getElementById('complete2').addEventListener('click',function(){
+
+inNumb++;
+numb--;
+alert("Board updated successfully");
+if(inNumb==29){
+alert("successful")
+}
+increaseNum=document.getElementById("decrease-number").innerText=numb;
+increaseNum=document.getElementById("increase-number").innerText=inNumb;
+const tite2=document.getElementById('title2').innerText;
+let now = new Date();
+
+const newComment='You have completed the task '+tite2+' issue at'+ now;
+
+
+const commentContainer=document.getElementById('comment-box');
+const newTransform=document.createElement('p');
+newTransform.classList.add('comment');
+newTransform.innerText=newComment;
+///new child
+commentContainer.appendChild(newTransform);
+
+let button = document.getElementById("complete2");
+button.disabled = true;
+button.classList.add("bg-gray-400", "cursor-not-allowed");
+
+
+
+
+})
+document.getElementById('complete1').addEventListener('click',function(){
+
+inNumb++;
+numb--;
+alert("Board updated successfully");
+if(inNumb==29){
+alert("successful")
+}
+increaseNum=document.getElementById("decrease-number").innerText=numb;
+increaseNum=document.getElementById("increase-number").innerText=inNumb;
+const tite1=document.getElementById('title1').innerText;
+let now = new Date();
+
+const newComment='You have completed the task '+tite1+' issue at'+ now;
+
+
+const commentContainer=document.getElementById('comment-box');
+const newTransform=document.createElement('p');
+newTransform.classList.add('comment');
+newTransform.innerText=newComment;
+///new child
+commentContainer.appendChild(newTransform);
+
+let button = document.getElementById("complete1");
+button.disabled = true;
+button.classList.add("bg-gray-400", "cursor-not-allowed");
+
+
+
+
+})
+document.getElementById('refresh').addEventListener('click',function(){
+// console.log(newComment)
+
+const commentContainer=document.getElementById('comment-box');
+commentContainer.innerText='';
+
+
+
+
+//get text from comment box
+
+//freeeee
+
+})
